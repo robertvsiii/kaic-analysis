4b35cfa21336589f213271a46901028e128988c2
robertvsiii <robertvsiii@gmail.com>
2018-07-09 10:13:51 -0400

    Use CII hydrolysis
    Accidentally left CII hydrolysis off last time

--- kaic_analysis/KaiC_calc_PCA.py
+++ kaic_analysis/KaiC_calc_PCA.py
@@ -18,11 +18,11 @@ folder = '/project/biophys/thermodynamics_of_oscillations'
 code_folder = '/usr2/postdoc/marsland/KMC_KaiC'
 #distutils.dir_util.mkpath(folder)
 
-ATPvec = 1-np.exp(np.linspace(-4,np.log(0.45),10))
+ATPvec = 1-np.exp(np.linspace(-4,np.log(0.4),10))
 param_val = ATPvec[args.task_ID-1]
 
 RunExperiment(ens_size=args.es,param_val=param_val,param_name='ATPfrac',code_folder=code_folder,
-              sample_cnt=int(args.sc),run_number=args.task_ID,use_PCA=True,CIIhyd=False)
+              sample_cnt=int(args.sc),run_number=args.task_ID,use_PCA=True,CIIhyd=True)
 
 tau, DelS, results = ProcessExperiment(run_number=args.task_ID,date=str(datetime.datetime.now()).split()[0],
                                        param_name='ATPfrac',param_val=param_val,folder=folder,
